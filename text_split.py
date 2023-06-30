504c5dd66788eabc91a5d30f2b58cd0c5ed65027
Shouyang Dong <shouyang.dong@gmail.com>
2023-06-30 11:56:25 +0800

    add text split

---
+++ text_split.py
@@ -0,0 +1,44 @@
+import numpy as np
+from typing import Iterator
+from numpy import array, average
+
+
+TEXT_EMBEDDING_CHUNK_SIZE=2048
+# Split a text into smaller chunks of size n, preferably ending at the end of a sentence
+def chunks(text, n, tokenizer):
+    """Yield successive n-sized chunks from text."""
+    tokens = tokenizer.encode(text)
+    i = 0
+    while i < len(tokens):
+        # Find the nearest end of sentence within a range of 0.5 * n and 1.5 * n tokens
+        j = min(i + int(1.5 * n), len(tokens))
+        while j > i + int(0.5 * n):
+            # Decode the tokens and check for full stop or newline
+            chunk = tokenizer.decode(tokens[i:j])
+            if chunk.endswith(".") or chunk.endswith("\n"):
+                break
+            j -= 1
+        # If no end of sentence found, use n tokens as the chunk size
+        if j == i + int(0.5 * n):
+            j = min(i + n, len(tokens))
+        yield tokens[i:j]
+        i = j
+
+
+def create_embeddings_for_text(text, tokenizer):
+    """Return a list of tuples (text_chunk, embedding) and an average embedding for a text."""
+    token_chunks = list(chunks(text, TEXT_EMBEDDING_CHUNK_SIZE, tokenizer))
+    text_chunks = [tokenizer.decode(chunk) for chunk in token_chunks]
+    return text_chunks
+
+def get_col_average_from_list_of_lists(list_of_lists):
+    """Return the average of each column in a list of lists."""
+    if len(list_of_lists) == 1:
+        return list_of_lists[0]
+    else:
+        list_of_lists_array = array(list_of_lists)
+        average_embedding = average(list_of_lists_array, axis=0)
+        return average_embedding.tolist()
+
+def get_embeddings(text_array, tokenizer):
+    return tokenizer.encode(text_array)
