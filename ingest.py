4a43ea20ad8a120fd5b2bd4f078aacb08154dd9d
Shouyang Dong <shouyang.dong@gmail.com>
2023-07-05 10:02:51 +0800

    update the introduction of gui

--- ingest.py
+++ ingest.py
@@ -3,8 +3,8 @@ import os
 import pickle
 from sentence_transformers import SentenceTransformer
 
-model_name = "all-MiniLM-L6-v2"
-model = SentenceTransformer(model_name)
+MODEL_NAME = "all-MiniLM-L6-v2"
+model = SentenceTransformer(MODEL_NAME)
 
 def save_database(embedding_cache_path, file_name):
     # Check if embedding cache path exists
